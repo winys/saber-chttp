dc5ee7836e6a6bc2d51ff2e38b6904692dc2b794
winys <winys@users.noreply.github.com>
2017-06-27 19:01:23 +0800

    release

--- build/dev-client.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-require('eventsource-polyfill')
-var hotClient = require('webpack-hot-middleware/client?path=http://localhost:9888/__webpack_hmr&noInfo=true&reload=true')
-
-hotClient.subscribe(function (event) {
-  if (event.action === 'reload') {
-    window.location.reload()
-  }
-})
